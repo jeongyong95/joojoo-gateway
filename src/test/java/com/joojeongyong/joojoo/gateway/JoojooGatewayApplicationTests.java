84a15f1746c480926b5d5629d47a53940fd87ccb
jeongyong <jeongyong95@gmail.com>
2022-11-30 17:53:31 +0900

    [GATEWAY-001] gateway eureka 연동

---
+++ src/test/java/com/joojeongyong/joojoo/gateway/JoojooGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joojeongyong.joojoo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoojooGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
